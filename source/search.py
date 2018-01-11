186b531463100af26e9939e62b1f53a405ac1d02
Sky Xu <skyrocks88xmz@gmail.com>
2018-01-11 13:48:02 -0800

    finish linear search recursive

--- source/search.py
+++ source/search.py
@@ -4,8 +4,10 @@ def linear_search(array, item):
     """return the first index of item in array or None if item is not found"""
     # implement linear_search_iterative and linear_search_recursive below, then
     # change this to call your implementation to verify it passes all tests
-    return linear_search_iterative(array, item)
-    # return linear_search_recursive(array, item)
+    # return linear_search_iterative(array, item)
+    # if item not in array:
+    #     raise ValueError('item not in the list')
+    return linear_search_recursive(array, item)
 
 
 def linear_search_iterative(array, item):
@@ -18,9 +20,13 @@ def linear_search_iterative(array, item):
 
 def linear_search_recursive(array, item, index=0):
     # TODO: implement linear search recursively here
-    pass
-    # once implemented, change linear_search to call linear_search_recursive
-    # to verify that your recursive implementation passes all tests
+    arr_length = len(array)
+    if index < arr_length:
+        if array[index] == item:
+            return index
+        else:
+            index += 1
+            return linear_search_recursive(array, item, index)
 
 
 def binary_search(array, item):
